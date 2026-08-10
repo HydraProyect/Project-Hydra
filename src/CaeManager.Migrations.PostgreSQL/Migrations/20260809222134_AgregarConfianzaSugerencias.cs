ef524e492d58b77d1289d24bd3444a5b3d9aef9b
christopherjp1-jpg <christopherjp1@gmail.com>
2026-08-10 02:16:33 +0200

    feat(comunicaciones): Action Center con revisión de sugerencias IA por campo (#175)

---
+++ src/CaeManager.Migrations.PostgreSQL/Migrations/20260809222134_AgregarConfianzaSugerencias.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CaeManager.Migrations.PostgreSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarConfianzaSugerencias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Confianza",
+                table: "SugerenciasVisitaCorreo",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Confianza",
+                table: "SugerenciasGestionCorreo",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Confianza",
+                table: "SugerenciasVisitaCorreo");
+
+            migrationBuilder.DropColumn(
+                name: "Confianza",
+                table: "SugerenciasGestionCorreo");
+        }
+    }
+}
